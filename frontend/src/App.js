3349fd14b66cda83749a9f710a33e1f461d6f0bd
Tayal01 <ltayal418@gmail.com>
2026-03-11 21:07:58 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     try {
       setLoading(true);
 
-      const response = await axios.post("https://firereach-q6mo.onrender.com", {
+      const response = await axios.post("https://firereach-q6mo.onrender.com/run-agent", {
         icp,
         company,
         email,
